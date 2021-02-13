91d9604ebc2ee5cde11a203cd2a9153fd2a0aa6d
hmsk <jojojojo1783@gmail.com>
2021-02-13 15:11:25 +0900

    button

--- web/app.py
+++ web/app.py
@@ -1,33 +1,51 @@
 
-from flask import Flask
+from flask import Flask, jsonify, redirect, render_template
 import subprocess
+import requests
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return '<a href="/on">on</a><br><a href="/off">off</a>'
+    return '<a href="/on_button">on</a><br><a href="/off_button">off</a>'
+
+
+@app.route('/on_button')
+def on_button():
+    res = requests.get('/on')
+    return redirect("/")
+
+
+@app.route('/off_button')
+def off_button():
+    res = requests.get('/off')
+    return redirect("/")
 
 
 @app.route('/on')
 def on():
-    return send_ir('light:on')
+    res = send_ir('light:on')
+    if res:
+        return jsonify({'status': False})
+    else:
+        return jsonify({'status': True})
 
 
 @app.route('/off')
 def off():
-    return send_ir('light:off')
+    res = send_ir('light:off')
+    if res:
+        return jsonify({'status': False})
+    else:
+        return jsonify({'status': True})
 
 
 def send_ir(name):
     command = ['python', 'irrp.py', '-p', '-g27',
                '-f', './config/val/val.json', name]
     res = subprocess.call(command)
-    if res:
-        return "{'status':False}"
-    else:
-        return "{'status':True}"
+    return res
 
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
